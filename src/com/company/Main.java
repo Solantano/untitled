b2df6b93708e494bdbe0f97a128082af2eceb412
Anton Solonovich <solantano@icloud.com>
2021-11-08 20:16:28 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,129 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        /*System.out.print ("Введите число:");
+        Scanner inputFigure = new Scanner(System.in);
+        int a = inputFigure.nextInt();
+
+        if ( a < 0 ) {
+            System.out.println (a=a-2);
+        }  else if (a==0) {
+            System.out.println (a=10);
+        }
+        else {
+            System.out.println (a=a+1);
+        }
+
+
+    }*/
+
+        /*int a = 10;
+        int b = 5;
+        int c = 2;
+
+        if (a < b && b < c)
+            System.out.println(a + " " + b + " " + c);
+
+            if (a < b && b < c){
+            sout(a)
+            if(b < c){
+            }
+            else {
+            }
+            }
+
+
+        else if (a < c && c < b)
+            System.out.println(a + " " + c + " " + b);
+
+        else if (b < a && a < c)
+            System.out.println(b + " " + a + " " + c);
+
+        else if (b < c && c < a)
+            System.out.println(b + " " + c + " " + a);
+
+        else if (c < a && a < b)
+            System.out.println(c + " " + a + " " + b);
+
+        else if (c < b && b < a)
+            System.out.println(c + " " + b + " " + a);*/
+
+
+        /*int a = 100;
+        int b = 7;
+        int c = -2;
+
+        if (a < b && b < c)
+            System.out.println(b);
+
+        else if (a < c && c < b)
+            System.out.println(c);
+
+        else if (b < a && a < c)
+            System.out.println(a);
+
+        else if (b < c && c < a)
+            System.out.println(c);
+
+        else if (c < a && a < b)
+            System.out.println(a);
+
+        else if (c < b && b < a)
+            System.out.println(b);*/
+
+        /*System.out.print ("Введите число:");
+        Scanner inputFigure = new Scanner(System.in);
+        int a = inputFigure.nextInt();
+        System.out.println(a*(a+1)/2);*/
+
+
+        /*for (int a = 7; a <= 98; a = a +7) ;
+        {
+            System.out.println(a +"");*/
+
+
+        /*for (int a = 1; a <= 512; a = a + a) {
+            System.out.print(a + " ");
+        }*/
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println ("Что такое синий большой, с усамии полностью забит зайцами?:");
+String answer = scanner.nextLine();
+        for (int a = 0; a < 3; a++); {
+        if (answer.equals("Троллейбус")) {
+                System.out.println("Правильно");
+                break;
+            }
+              else   if (answer.equals("Сдаюсь")) {
+                    System.out.println("Правильный ответ:троллейбус");
+              break;}
+                else {
+                    System.out.println("неправильный ответ, подумай");
+                    scanner.nextLine();
+                    System.out.println("неправильный ответ, подумай");
+                    scanner.nextLine();
+                    System.out.println("неправильный ответ");
+                    System.out.println("Правильный ответ:троллейбус");}
+                scanner.nextLine();}*/
+        /*int array[] = {-99999999, -99999993, 65, 5, 5656, 89, 4, 8, 2, 4343, 0};
+        int minus = 0;
+        int i;
+        for (i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                minus = array[i];
+                System.out.println(minus);
+            }*/
+
+    }
+}
+
+
+
+
+
+
+
+
+
